04a6f4f5ed917bcf29bba22f72bdd5a816e3bdb0
conf8o <41929738+conf8o@users.noreply.github.com>
2023-04-08 23:28:23 +0900

    ソロ、デュオをマッチングシステムを追加

--- matching_system/app/domain/matching.py
+++ matching_system/app/domain/matching.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from numbers import Number
 
 @dataclass
 class Player:
@@ -7,6 +8,7 @@ class Player:
     """
 
     id: str
+    name: str
     point: int
 
     @property
@@ -25,6 +27,16 @@ class Entry:
             raise PartyMaxLenError()
         
         self.players = players
+
+    @property
+    def inner_rate(self) -> int:
+        return sum(p.inner_rate for p in self.players) / len(self.players)
+
+    def sort_key(self) -> tuple[int]:
+        return [self.inner_rate, *sorted(p.inner_rate for p in self.players)]
+    
+    def __repr__(self) -> str:
+        return f"Entry({self.players})"
 
 
 class PartyMaxLenError(Exception):
@@ -53,5 +65,170 @@ class Matching:
     parties: list[Party]
 
 
-def calculate_matching(players: list[Entry]) -> Matching:
-    return False
+def _median(ns: list[Number]) -> Number:
+    """
+    [1, 2, 3, 4] -> 2.5
+    [1, 2, 3] -> 2
+    """
+
+    l = len(ns)
+    if l % 2 == 0:
+        return (ns[l//2] + ns[l//2-1]) / 2
+    else:
+        return ns[l//2-1]
+
+class MakeTrioModeError(Exception):
+    pass
+
+class SoloMatching:
+    def __init__(self, entries: list[Entry]) -> None:
+        self.entries = list(sorted(entries, key=Entry.sort_key))
+        self.players = [entry.players[0] for entry in self.entries]
+
+    def make_match(self) -> list[Entry]:
+        """
+        [3, 3, 3, 4, 4, 4, 5, 6, 6, 6, 6, 7, 7, 7]: len = 13, split_len = 13 // 3 = 4 
+        
+        matching_queues:
+        [3, 3, 3, 4] lower sorted asc
+        [7, 7, 7, 6] higher sorted desc
+        [4, 4, 5, 6] mid sorted asc
+        
+        ↑ 列でマッチ
+        rest: [6, 6]
+        """
+
+        l = len(self.players)
+        if l < 3:
+            return self.entries
+        
+        split_l = l // 3
+        lower = self.players[:split_l]
+        higher = sorted(self.players[split_l:], key=lambda x: -x.inner_rate)
+        
+        mid = self.players[split_l:-split_l]
+        rest = [] if split_l == len(mid) else [Entry(mid[split_l:])]
+        mid = mid[:split_l]
+
+        matchings = [Entry(list(trio)) for trio in zip(lower, higher, mid)]
+
+        return matchings + rest
+
+
+def lowers(entries):
+    return entries[:len(entries)//2]
+    
+
+def highers(entries):
+    return entries[len(entries)//2:]
+    
+class DuoMatching:
+    def make_trio(duo: Entry, solo: Entry) -> Entry:
+        return Entry([duo.players[0], duo.players[1], solo.players[0]])
+
+    def __init__(self, entries: list[Entry]) -> None:
+        self.entries = sorted(entries, key=Entry.sort_key)
+
+        self.solos = [entry for entry in self.entries if len(entry.players) == 1]
+        self.duos = [entry for entry in self.entries if len(entry.players) == 2]
+
+        self.lower_solos = lowers(self.solos)
+        self.higher_solos = highers(self.solos)
+        self.lower_duos = lowers(self.duos)
+        self.higher_duos = highers(self.duos)
+
+        input(self.lower_solos)
+        input(self.higher_solos)
+        input(self.lower_duos)
+        input(self.higher_duos)
+
+    def make_match(self) -> list[Entry]:
+        """
+        entries:
+            [[2], [2], [2, 2], [3], [2, 4], [3, 3], [5]]
+        
+        1. lower_duo_matching:
+            [[2, 2], [3], [5]]
+        2. higher_duo_matching:
+            [[2], [2], [2, 4], [3, 3]]
+        
+        1-1. lower_duo_matching:
+            [[2, 2, 5], [3]]
+        1-2. higher_duo_matching:
+            pass
+            
+        2-1. lower_duo_matching:
+            [[2, 4, 2]]
+        2-2. higher_duo_matching:
+            [[3, 3, 2]]
+        """
+
+        matchings = []
+
+        # lower_duo_matching
+        if len(self.lower_duos) == 1:
+            solo = self.higher_solos[-1]
+            solos = [] if len(self.higher_solos) == 1 else self.higher_solos[:-1]
+            trio = DuoMatching.make_trio(self.lower_duos[0], solo)
+            matchings += [trio, *solos]
+        else:
+            if not self.lower_duos:
+                matchings += self.higher_solos
+            elif not self.higher_solos:
+                matchings += self.lower_duos
+            else:
+                lower_duo_matching = DuoMatching(self.lower_duos + self.higher_solos)
+                matchings += lower_duo_matching.make_match()
+        
+        # higher_duo_matching
+        if len(self.higher_duos) == 1:
+            solo = self.lower_solos[0]
+            solos = [] if len(self.lower_solos) == 1 else self.lower_solos[1:]
+            trio = DuoMatching.make_trio(self.higher_duos[0], solo)
+            matchings += [trio, *solos]
+        else:
+            if not self.higher_duos:
+                matchings += self.lower_solos
+            elif not self.lower_solos:
+                matchings += self.higher_duos
+            else:
+                higher_duo_matching = DuoMatching(self.higher_duos + self.lower_solos)
+                matchings += higher_duo_matching.make_match()
+
+        return matchings
+
+
+def calculate_matching(entries: list[Entry]) -> list[Entry]:
+    entries = list(sorted(entries, key=lambda e: e.inner_rate))
+
+    mid = _median([entry.inner_rate for entry in entries])
+
+    solos= [entry for entry in entries if len(entry.players) == 1]
+    duos = [entry for entry in entries if len(entry.players) == 2]
+    trios = [entry for entry in entries if len(entry.players) == 3]
+
+    def lowers(entries):
+        return [entry.inner_rate for entry in entries if entry.inner_rate <= mid]
+    
+    def highers(entries):
+        return [entry.inner_rate for entry in entries if entry.inner_rate > mid]
+    
+    lower_solos = lowers(solos)
+    higher_solos = highers(solos)
+
+    lower_duos = lowers(duos)
+    higher_duos = highers(duos)
+
+    new_trios = []
+    if len(higher_duos) == 1:
+        trio = _make_trio(higher_duos[0], lower_solos[0])
+        new_trios.append(trio)
+
+    if len(lower_duos) == 1:
+        trio = _make_trio(lower_duos[0], higher_solos[-1])
+        new_trios.append(trio)
+
+    
+    
+    calculate_matching(lower_solos + higher_duos)
+    calculate_matching(higher_solos + lower_solos)
--- matching_system/tests/domain/test_matching.py
+++ matching_system/tests/domain/test_matching.py
@@ -1,9 +1,13 @@
 from app.domain import matching
 
 
+players = [matching.Player("1", "a", 10),
+           matching.Player("2", "b", 20),
+           matching.Player("3", "c", 30)]
+
 def test_player_innner_rate():
     point = 20
-    player = matching.Player("1", point)
+    player = matching.Player("1", "a", point)
     assert 20 == player.inner_rate
 
 
@@ -12,9 +16,66 @@ def test_party_inner_rate():
     point2 = 5
     point3 = 100
     party = matching.Party(
-        [matching.Player("1", point1),
-         matching.Player("2", point2),
-         matching.Player("3", point3)]
+        [matching.Player("1", "a", point1),
+         matching.Player("2", "b", point2),
+         matching.Player("3", "c", point3)]
     )
 
     assert sum([point1, point2, point3]) / 3 == party.inner_rate
+
+def test_solo_matching():
+    solos = [matching.Entry([matching.Player("1", "b", 3)]),
+             matching.Entry([matching.Player("2", "b", 3)]),
+             matching.Entry([matching.Player("3", "b", 3)]),
+             
+             matching.Entry([matching.Player("4", "b", 4)]),
+             matching.Entry([matching.Player("5", "b", 4)]),
+             matching.Entry([matching.Player("6", "b", 4)]),
+             
+             matching.Entry([matching.Player("7", "b", 5)]),
+             matching.Entry([matching.Player("8", "b", 6)]),
+             matching.Entry([matching.Player("9", "b", 6)]),
+             
+             matching.Entry([matching.Player("10", "b", 6)]),
+             matching.Entry([matching.Player("11", "b", 6)]),
+             matching.Entry([matching.Player("12", "b", 7)]),
+             
+             matching.Entry([matching.Player("13", "b", 7)]),
+             matching.Entry([matching.Player("14", "b", 7)])]
+        
+    solo_matching = matching.SoloMatching(solos)
+    actual = solo_matching.make_match()
+    expected = [[3, 7, 4],
+                [3, 7, 4],
+                [3, 7, 5],
+                [4, 6, 6],
+                [6, 6]]
+    
+    assert expected == [[p.inner_rate for p in e.players] for e in actual]
+
+
+def test_duo_matching():
+    entries = [matching.Entry([matching.Player("1", "b", 2)]),
+               matching.Entry([matching.Player("2", "b", 2)]),
+               
+               matching.Entry([matching.Player("3", "b", 2),
+                               matching.Player("4", "b", 2)]),
+                               
+               matching.Entry([matching.Player("5", "b", 3)]),
+               
+               matching.Entry([matching.Player("6", "b", 2),
+                               matching.Player("7", "b", 4)]),
+                               
+               
+               matching.Entry([matching.Player("8", "b", 3),
+                               matching.Player("9", "b", 3)]),
+                               
+               matching.Entry([matching.Player("10", "b", 5)])]
+    
+    duo_matching = matching.DuoMatching(entries)
+    actual = duo_matching.make_match()
+    expected = set([(2, 2, 5),
+                    (3,),
+                    (2, 4, 2),
+                    (3, 3, 2)])
+    assert expected == set(tuple(p.inner_rate for p in e.players) for e in actual)
