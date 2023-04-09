8df97ad92e5d759b16740c6adc9e3718595a52d4
conf8o <41929738+conf8o@users.noreply.github.com>
2023-04-09 16:05:09 +0900

    エンドポイントの追加

--- matching_system/app/endpoints/matching.py
+++ matching_system/app/endpoints/matching.py
@@ -1,16 +1,25 @@
 from typing import Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database import get_db
+from app.database import get_db
 
 router = APIRouter()
 
-@router.get("/")
+
+@router.post("/entries")
+def create_entry(db: Session = Depends(get_db)):
+    return ""
+
+@router.get("/entries")
+def query_entry(db: Session = Depends(get_db)):
+    return ""
+
+
+@router.get("/matching")
+def query_matching(db: Session = Depends(get_db)):
+    return ""
+
+@router.post("/match_making")
 def read_root(db: Session = Depends(get_db)):
 
     return str(db)
-
-
-@router.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
